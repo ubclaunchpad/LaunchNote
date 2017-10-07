589f5ec3bc1f4f7ca4b178ad16d2c05be8234acf
Andres R. Torres <rupertotorres1@gmail.com>
2017-10-07 15:46:21 -0700

    implementing basic functions of photo-taking

--- app/src/main/java/com/example/sherryuan/launchnote/TakePhotoActivity.java
+++ app/src/main/java/com/example/sherryuan/launchnote/TakePhotoActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class TakePhotoActivity extends AppCompatActivity {
 
@@ -15,7 +16,7 @@ public class TakePhotoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_take_photo);
     }
 
-    private void takePhoto() {
+    private void takePhoto(View view) {
         Intent takePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePhotoIntent.resolveActivity(getPackageManager()) != null) {
             startActivityForResult(takePhotoIntent, TAKE_PHOTO_REQUEST_CODE);
