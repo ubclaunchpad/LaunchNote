4d9ea5a5fe7d7f0a4cc07b3ea676feac3c18adc7
Sherry Yuan <frostyyshadows@gmail.com>
2017-10-14 13:47:43 -0700

    added glide

--- app/src/main/java/com/example/ubclaunchpad/launchnote/gallery/GalleryActivity.java
+++ app/src/main/java/com/example/ubclaunchpad/launchnote/gallery/GalleryActivity.java
@@ -3,6 +3,7 @@ package com.example.ubclaunchpad.launchnote.gallery;
 import android.app.Activity;
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
@@ -11,21 +12,25 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.SimpleTarget;
+import com.bumptech.glide.request.transition.Transition;
 import com.example.ubclaunchpad.launchnote.R;
 
 /**
- * Created by Beriwan on 2017-09-30.
+ * Activity for selecting an image from photo gallery
  */
-
 public class GalleryActivity extends Activity {
 
     private static int RESULT_LOAD_IMG = 1;
-    String imgDecodableString;
+    ImageView photoView;
+    Bitmap photoBitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gallery);
+        photoView = findViewById(R.id.imgView);
 
     }
 
@@ -38,34 +43,27 @@ public class GalleryActivity extends Activity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        try {
-            // when an Image is picked
-            if (requestCode == RESULT_LOAD_IMG && resultCode == RESULT_OK && null != data) {
-                // get Image from data
-                Uri selectedImage = data.getData();
-                String[] filePathColumn = {MediaStore.Images.Media.DATA};
+        // when an Image is picked
+        if (requestCode == RESULT_LOAD_IMG && resultCode == RESULT_OK && null != data) {
+            // get Image from data
+            Uri selectedImage = data.getData();
+            // load Bitmap using Glide
+            Glide.with(this)
+                    .asBitmap()
+                    .load(selectedImage)
+                    .into(new SimpleTarget<Bitmap>() {
+                        @Override
+                        public void onResourceReady(Bitmap resource, Transition transition) {
+                            // have photoView display the loaded Bitmap
+                            photoView.setImageBitmap(resource);
+                            // set photoBitmap to the loaded Bitmap
+                            photoBitmap = resource;
+                        }
+                    });
 
-                // Get the cursor
-                Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+        } else {
+            Toast.makeText(this, "You haven't picked Image", Toast.LENGTH_LONG).show();
 
-                // Move to first row
-                cursor.moveToFirst();
-
-                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-                imgDecodableString = cursor.getString(columnIndex);
-                cursor.close();
-                ImageView imgView = findViewById(R.id.imgView);
-
-                // Set the Image in ImageView after decoding the String
-                imgView.setImageBitmap(BitmapFactory.decodeFile(imgDecodableString));
-
-            } else {
-                Toast.makeText(this, "You haven't picked Image", Toast.LENGTH_LONG).show();
-
-            }
-
-        } catch (Exception e) {
-            Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG).show();
         }
     }
 
