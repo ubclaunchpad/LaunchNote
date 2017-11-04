2c5ef0d5564d8871fbc17c4a5e1fc5ba2b9d8023
Sherry Yuan <frostyyshadows@gmail.com>
2017-11-04 12:49:32 -0700

    some changes

--- app/src/main/java/com/example/ubclaunchpad/launchnote/BaseActivity.kt
+++ app/src/main/java/com/example/ubclaunchpad/launchnote/BaseActivity.kt
@@ -1,13 +1,14 @@
 package com.example.ubclaunchpad.launchnote
 
 import android.content.Intent
+import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
 import android.os.Bundle
-import android.support.design.internal.BottomNavigationMenu
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
-import butterknife.BindView
+import android.widget.PopupMenu
+import com.example.ubclaunchpad.launchnote.addPhoto.GalleryActivity
+import com.example.ubclaunchpad.launchnote.addPhoto.TakePhotoActivity
 import com.example.ubclaunchpad.launchnote.photoBrowser.PhotoBrowserActivity
-import kotlinx.android.synthetic.main.activity_base.*
 
 
 abstract class BaseActivity : AppCompatActivity() {
@@ -22,18 +23,45 @@ abstract class BaseActivity : AppCompatActivity() {
         bottomNavigation.setOnNavigationItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.add_menu_item -> {
-
+                    onAddPhotoClick()
                 }
                 R.id.scan_menu_item -> {
 
                 }
                 R.id.browse_menu_item -> {
-                    startActivity(Intent(applicationContext, PhotoBrowserActivity::class.java))
+                    val photoBrowserActivityIntent = Intent(applicationContext, PhotoBrowserActivity::class.java)
+                            .apply { flags = FLAG_ACTIVITY_NEW_TASK }
+                    startActivity(photoBrowserActivityIntent)
                 }
             }
             true
         }
     }
+
+    /**
+     * When "Add Photo" is clicked, show menu allowing users to choose between
+     * uploading from gallery and taking a photo
+     */
+    fun onAddPhotoClick() =
+            PopupMenu(applicationContext, bottomNavigation).apply {
+                //Inflating the Popup using xml file
+                menuInflater.inflate(R.menu.add_photo, this.menu)
+
+                //registering popup with OnMenuItemClickListener
+                setOnMenuItemClickListener { item ->
+                    if (item.itemId == R.id.take_photo_item) {
+                        val takePhotoActivityIntent = Intent(applicationContext, TakePhotoActivity::class.java)
+                                .apply { flags = FLAG_ACTIVITY_NEW_TASK }
+                        startActivity(takePhotoActivityIntent)
+                    } else if (item.itemId == R.id.add_from_library_item) {
+                        val galleryActivityIntent = Intent(applicationContext, GalleryActivity::class.java)
+                                .apply { flags = FLAG_ACTIVITY_NEW_TASK }
+                        startActivity(galleryActivityIntent)
+                    }
+                    true
+                }
+                show()
+            }
 
     abstract fun getContentViewId(): Int
 }
