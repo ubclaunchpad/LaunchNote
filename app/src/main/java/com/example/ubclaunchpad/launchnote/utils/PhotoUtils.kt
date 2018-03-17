74aea176f58cb2a17576157b2e7f86ec5cb015ea
Sherry Yuan <frostyyshadows@gmail.com>
2018-03-17 13:59:31 -0700

    formatting

--- app/src/main/java/com/example/ubclaunchpad/launchnote/utils/PhotoUtils.kt
+++ app/src/main/java/com/example/ubclaunchpad/launchnote/utils/PhotoUtils.kt
@@ -16,7 +16,7 @@ import java.io.File
 import java.io.FileOutputStream
 import java.io.IOException
 import java.text.SimpleDateFormat
-import java.util.*
+import java.util.Date
 
 
 object PhotoUtils {
