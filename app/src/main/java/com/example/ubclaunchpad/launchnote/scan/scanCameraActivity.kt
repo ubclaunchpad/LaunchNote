9f8da25b945902c061e14993f44ba2ca98c10e0e
ruperto <rupertotorres1@gmail.com>
2018-03-17 14:53:52 -0700

    Worked the first time after clean installation, black screen afterwards.

--- app/src/main/java/com/example/ubclaunchpad/launchnote/scan/scanCameraActivity.kt
+++ app/src/main/java/com/example/ubclaunchpad/launchnote/scan/scanCameraActivity.kt
@@ -2,8 +2,9 @@ package com.example.ubclaunchpad.launchnote.scan
 
 import android.annotation.SuppressLint
 import android.content.Context
-import android.hardware.camera2.CameraDevice
-import android.hardware.camera2.CameraManager
+import android.content.pm.PackageManager
+import android.graphics.SurfaceTexture
+import android.hardware.camera2.*
 import android.os.Build
 import android.os.Bundle
 import android.support.design.widget.Snackbar
@@ -12,23 +13,186 @@ import android.util.Log
 import com.example.ubclaunchpad.launchnote.R
 
 import kotlinx.android.synthetic.main.activity_scan_camera.*
+import android.view.TextureView
+import android.util.Size
+import android.hardware.camera2.params.StreamConfigurationMap
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.hardware.camera2.CameraAccessException
+import android.hardware.camera2.CameraDevice
+import android.opengl.ETC1.getHeight
+import android.opengl.ETC1.getWidth
+import android.view.Surface
+import android.hardware.camera2.CameraCaptureSession
+import android.os.HandlerThread
+import android.hardware.camera2.CameraMetadata
+import android.hardware.camera2.CaptureRequest
+import android.os.Handler
+
 
 class ScanCameraActivity : AppCompatActivity() {
 
+    companion object {
+        const val TAG = "SimpleCamera"
+        const val MY_PERMISSIONS_REQUEST_CAMERA = 123
+    }
+
+    private lateinit var mTextureView: TextureView
+    private lateinit var mPreviewSize: Size
+    private var mCameraDevice: CameraDevice? = null
+    private lateinit var mPreviewBuilder: CaptureRequest.Builder
+    private lateinit var mPreviewSession: CameraCaptureSession
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scan_camera)
-        setSupportActionBar(toolbar)
-
-        }
+        setSupportActionBar(toolbar) // remove this?
+    }
 
     override fun onResume() {
         super.onResume()
-        safeCameraOpen()
+        mTextureView = findViewById<TextureView>(R.id.previewTextureView)
+        mTextureView.setSurfaceTextureListener(mSurfaceTextureListner)
     }
 
-    private fun safeCameraOpen(): Boolean {
+    override fun onPause() {
+        // TODO Auto-generated method stub
+        super.onPause()
+
+        if (mCameraDevice != null) {
+            mCameraDevice?.close()
+            mCameraDevice = null
+        }
+    }
+
+    private var mSurfaceTextureListner: TextureView.SurfaceTextureListener = object: TextureView.SurfaceTextureListener {
+        override fun onSurfaceTextureSizeChanged(surface: SurfaceTexture?, width: Int, height: Int) {
+            //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            Log.i(TAG, "onSurfaceTextureSizeChanged()")
+        }
+
+        override fun onSurfaceTextureUpdated(surface: SurfaceTexture?) {
+            //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            Log.i(TAG, "onSurfaceTextureUpdated()")
+        }
+
+        override fun onSurfaceTextureDestroyed(surface: SurfaceTexture?): Boolean {
+            //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            Log.i(TAG, "onSurfaceTextureDestroyed()")
+            return false
+        }
+
+        override fun onSurfaceTextureAvailable(surface: SurfaceTexture?, width: Int, height: Int) {
+            Log.i(TAG, "onSurfaceTextureAvailable()")
+            // Do I really need to do this check?
+            if (ContextCompat.checkSelfPermission(this@ScanCameraActivity, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this@ScanCameraActivity, arrayOf(android.Manifest.permission.CAMERA), MY_PERMISSIONS_REQUEST_CAMERA)
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            MY_PERMISSIONS_REQUEST_CAMERA -> {
+                // If request is cancelled, the result arrays are empty.
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    // permission granted
+                    safeCameraOpen()
+                } else {
+                    // permission denied
+                }
+                return
+            }
+
+            else -> {}
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun safeCameraOpen() {
+        val manager : CameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
+        try {
+            val cameraId = manager.cameraIdList[0]
+            val characteristics = manager.getCameraCharacteristics(cameraId)
+            val map = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP)
+            mPreviewSize = map.getOutputSizes(SurfaceTexture::class.java)[0]
+            manager.openCamera(cameraId, mStateCallback, null)
+        }
+        catch (e: CameraAccessException) {
+            e.printStackTrace()
+        }
+    }
+
+    private var mStateCallback: CameraDevice.StateCallback = object: CameraDevice.StateCallback() {
+        override fun onOpened(camera: CameraDevice?) {
+            Log.i(TAG, "onOpened")
+            mCameraDevice = camera
+            val texture = mTextureView.surfaceTexture
+            if (texture == null) {
+                Log.e(TAG, "texture is null")
+                return
+            }
+
+            texture.setDefaultBufferSize(mPreviewSize.width, mPreviewSize.height)
+            val surface = Surface(texture)
+
+            try {
+                mPreviewBuilder = mCameraDevice!!.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
+            } catch (e: CameraAccessException) {
+                e.printStackTrace()
+            }
+
+
+            mPreviewBuilder.addTarget(surface)
+
+            try {
+                mCameraDevice!!.createCaptureSession(arrayListOf(surface), mPreviewStateCallback, null)
+            } catch (e: CameraAccessException) {
+                e.printStackTrace()
+            }
+
+        }
+
+        override fun onDisconnected(camera: CameraDevice?) {
+            //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            Log.e(TAG, "onDisconnected");
+        }
+
+        override fun onError(camera: CameraDevice?, int: Int) {
+            //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            Log.e(TAG, "onError")
+        }
+    }
+
+    private val mPreviewStateCallback = object : CameraCaptureSession.StateCallback() {
+
+        override fun onConfigured(session: CameraCaptureSession) {
+            // TODO Auto-generated method stub
+            Log.i(TAG, "onConfigured")
+            mPreviewSession = session
+
+            mPreviewBuilder.set(CaptureRequest.CONTROL_MODE, CameraMetadata.CONTROL_MODE_AUTO)
+
+            val backgroundThread = HandlerThread("CameraPreview")
+            backgroundThread.start()
+            val backgroundHandler = Handler(backgroundThread.looper)
+
+            try {
+                mPreviewSession.setRepeatingRequest(mPreviewBuilder.build(), null, backgroundHandler)
+            } catch (e: CameraAccessException) {
+                e.printStackTrace()
+            }
+
+        }
+
+        override fun onConfigureFailed(session: CameraCaptureSession) {
+            // TODO Auto-generated method stub
+            Log.e(TAG, "CameraCaptureSession Configure failed")
+        }
+    }
+
+
+    /*private fun safeCameraOpen(): Boolean {
         var manager : CameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
         var cameraIds = manager.cameraIdList
         var cameraCallback = object: CameraDevice.StateCallback() {
@@ -58,16 +222,9 @@ class ScanCameraActivity : AppCompatActivity() {
         }
 
         return true
-    }
-
-
-    /*private fun releaseCameraAndPreview() {
-        mPreview.setCamera(null)
-        if (mCamera != null) {
-            mCamera.release()
-            mCamera = null
-        }
     }*/
+
+
 
 }
 
